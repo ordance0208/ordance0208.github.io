e81bb8d9905085811bb3a76803c43ec73eed899f
Ordan Gramatov <ordance01@yahoo.com>
2023-11-22 19:00:21 +0100

    Implemented lists of work experiences and skills

---
+++ src/utils/experienceList.ts
@@ -0,0 +1,37 @@
+import Job from '../models/Job';
+
+const experienceList: Job[] = [
+  {
+    role: 'Front-End Developer',
+    employer: 'Lasting Dynamics SLU',
+    type: 'Internship',
+    arrangement: 'remote',
+    description: [
+      'Front End Development in React.js',
+      'ES06',
+      'Typescript',
+      'Agile Methodologies',
+      'Design Pattern and Software Architectures',
+      'Automated Tests',
+    ],
+    startDate: 'June 2022',
+    endDate: 'October 2022',
+  },
+  {
+    role: 'Front-End Developer',
+    employer: 'Lasting Dynamics SLU',
+    type: 'Full Time',
+    arrangement: 'remote',
+    description: [
+      'Worked on developing and delivering different kinds of milestones within an agile environment.',
+      'Collaborated closely with UI/UX designers to integrate their creative ideas into the projects.',
+      'Actively participated in code reviews, maintaining code quality and providing constructive feedback to team members.',
+      'Collaborated closely with the backend team to integrate and consume their APIs, ensuring seamless communication and data exchange between the frontend and backend systems.',
+      'Conducted thorough testing and debugging of software to identify and fix reported bugs',
+    ],
+    startDate: 'October 2022',
+    endDate: 'July 202',
+  },
+];
+
+export default experienceList;
---
+++ src/utils/skills.ts
@@ -0,0 +1,64 @@
+import Html from '../assets/svg/Html';
+import Css from '../assets/svg/Css';
+import Tailwind from '../assets/svg/Tailwind';
+import Mui from '../assets/svg/Mui';
+import Javascript from '../assets/svg/JavaScript';
+import Typescript from '../assets/svg/Typescript';
+import React from '../assets/svg/React';
+import Node from '../assets/svg/Node';
+import Express from '../assets/svg/Express';
+import Mongo from '../assets/svg/Mongo';
+import Git from '../assets/svg/Git';
+
+const skills = [
+  {
+    name: 'HTML',
+    icon: Html,
+  },
+  {
+    name: 'CSS',
+    icon: Css,
+  },
+  {
+    name: 'Tailwind',
+    icon: Tailwind,
+  },
+  {
+    name: 'Mui',
+    icon: Mui,
+  },
+  {
+    name: 'JavaScript',
+    icon: Javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: Typescript,
+  },
+  {
+    name: 'React & React Native',
+    icon: React,
+  },
+  {
+    name: 'Node',
+    icon: Node,
+  },
+  {
+    name: 'Express',
+    icon: Express,
+  },
+  {
+    name: 'MongoDB',
+    icon: Mongo,
+  },
+  {
+    name: 'Firebase',
+    icon: Mongo,
+  },
+  {
+    name: 'Git',
+    icon: Git,
+  },
+];
+
+export default skills;
