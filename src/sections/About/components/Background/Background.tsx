b171ed9ff93a05b1d6f06029a85fcb3480f8b2bf
Ordan Gramatov <ordance01@yahoo.com>
2023-11-22 18:59:07 +0100

    Abstracted section backgrounds in own components

---
+++ src/sections/About/components/Background/Background.tsx
@@ -0,0 +1,10 @@
+const Background = () => {
+  return (
+    <>
+      <div className='w-[400px] h-[400px] bg-[#eeeeee] absolute top-[50%] left-[70%] blur-[40px] z-0 opacity-5 rounded-full'></div>
+      <div className='w-[400px] h-[400px] bg-[#eeeeee] absolute top-[20%] left-[20%] blur-[40px] z-0 opacity-5 rounded-full'></div>
+    </>
+  );
+};
+
+export default Background;
--- src/sections/Hero/components/Background/Background.tsx
+++ src/sections/Hero/components/Background/Background.tsx
@@ -1,8 +1,8 @@
 const Background = () => {
   return (
     <>
-      <div className='w-[400px] h-[400px] bg-[#eeeeee] absolute top-[18%] left-[25%] blur-[40px] z-1 opacity-10 rounded-full'></div>
-      <div className='w-[300px] h-[300px] bg-[#eeeeee] absolute top-[70%] left-[60%] blur-[40px] z-1 opacity-10 rounded-full'></div>
+      <div className='w-[400px] h-[400px] bg-dark dark:bg-light absolute top-[18%] left-[25%] blur-[40px] z-1 opacity-[0.15] dark:opacity-10 rounded-full'></div>
+      <div className='w-[300px] h-[300px] bg-dark dark:bg-light absolute top-[60%] left-[60%] blur-[40px] z-1 opacity-[0.15] dark:opacity-10 rounded-full'></div>
       <div className='flex gap-20 absolute top-[60%] left-[10%] animate-left-right'>
         <p className='text-[220px] whitespace-nowrap text-accent opacity-10 font-bold'>
           Full-Stack Developer
