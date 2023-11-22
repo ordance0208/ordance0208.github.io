275746cbac3eb2f8484084ad675ba22964f4675f
Ordan Gramatov <ordance01@yahoo.com>
2023-11-22 18:43:23 +0100

    Refactored hero stylings

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,9 +1,9 @@
 import { useRef } from 'react';
 import Button from '../../components/Button';
-import avatar from '../../assets/img/avatar.png';
 import me from '../../assets/img/me.jpg';
 import Scroller from './components/Scroller';
 import Background from './components/Background';
+import Typograpghy from '../../components/Typography/Typograpghy';
 
 const Hero = () => {
   const content = useRef<HTMLDivElement>(null);
@@ -21,27 +21,33 @@ const Hero = () => {
 
   return (
     <section
-      className='tall:pt-[100px] tall:pb-[100px] tall:h-auto hero h-screen bg-[#090e19] relative overflow-hidden pt-[56px]'
+      className='tall:py-[100px] tall:h-auto h-screen bg-light dark:bg-[#090e19] relative overflow-hidden pt-[56px]'
       onMouseMove={handleMouseMove}
     >
       <Background />
       <div
-        className='animate-fly-in flex gap-10 justify-center items-center flex-col lg:flex-row 2xl:justify-between xl:justify-between h-full max-w-[90%] m-auto 2xl:max-w-[1440px] 2xl:flex-row xl:flex-row xl:max-w-[1144px] tall:justify-normal'
+        className='animate-fly-in flex gap-10 justify-center items-center flex-col h-full max-w-[90%] m-auto lg:flex-row xl:justify-between xl:max-w-[1144px] xl:flex-row 2xl:max-w-[1440px] tall:justify-normal'
         ref={content}
       >
-        <div className='w-[100%] 2xl:max-w-[720px] xl:max-w-[720px] lg:flex-1'>
-          <p className='text-[#eeeeee] text-center leading-[32px] text-[24px] font-bold mb-1 lg:text-left lg:text-[28px] 2xl:text-left xl:text-left 2xl:text-[56px] 2xl:leading-[56px] xl:leading-[56px] xl:text-[56px] 2xl:mb-0 xl:mb-0'>
+        <div className='w-[100%] lg:flex-1 xl:max-w-[720px]'>
+          <Typograpghy
+            variant='h1'
+            className='text-center leading-[32px] text-[24px] font-bold mb-1 lg:text-left lg:text-[28px] xl:leading-[56px] xl:text-[56px] xl:mb-0'
+          >
             Hi, my name is <span className='text-accent'>Ordan.</span>
-          </p>
-          <h3 className='text-[#eeeeee] text-center text-[20px] lg:text-left xl:text-left 2xl:text-left 2xl:text-[36px] xl:text-[36px] font-semibold mb-3'>
+          </Typograpghy>
+          <Typograpghy
+            variant='h3'
+            className='text-center text-[20px] font-semibold mb-3 lg:text-left xl:text-[36px]'
+          >
             I am a <span className='text-accent'>Full-Stack</span> Developer.
             💻📱
-          </h3>
-          <p className='text-[#eeeeee] text-center text-[14px] lg:text-left 2xl:text-left xl:text-left 2xl:text-[18px] xl:text-[18px] xs:text-[18px] font-normal'>
+          </Typograpghy>
+          <Typograpghy className='text-center text-[14px] font-normal lg:text-left xl:text-[18px] xs:text-[18px]'>
             From designing elegant front-end interfaces to architecting robust
             back-end systems, I thrive on transforming ideas into innovative and
             user-centric solutions. Let's code the future together! 🚀
-          </p>
+          </Typograpghy>
           <div className='flex gap-5 mt-4 justify-center lg:justify-start'>
             <Button className='px-2 py-1 text-[15px]'>Get in touch</Button>
             <Button className='px-1 py-1 text-[15px]' variant='secondary'>
@@ -51,8 +57,8 @@ const Hero = () => {
         </div>
         <div>
           <img
-            className='rounded-full w-[250px] h-[250px] 2xl:w-[500px] 2xl:h-[500px] select-none xl:w-[350px] xl:h-[350px] animate-grow border-4 border-white relative z-[1]'
-            src={avatar}
+            className='rounded-full w-[250px] h-[250px] relative z-[1] border-accent dark:border-white 2xl:w-[500px] 2xl:h-[500px] select-none xl:w-[350px] xl:h-[350px] animate-grow border-4'
+            src={me}
             alt='me'
           />
         </div>
