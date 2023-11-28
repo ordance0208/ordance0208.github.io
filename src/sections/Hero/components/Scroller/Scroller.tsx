1d21962f402b1483ed45510241281a7eedad5c41
Ordan Gramatov <ordance01@yahoo.com>
2023-11-28 13:36:06 +0100

    Hide scroller on mobile

--- src/sections/Hero/components/Scroller/Scroller.tsx
+++ src/sections/Hero/components/Scroller/Scroller.tsx
@@ -2,16 +2,12 @@ import { UilMouseAlt, UilAngleDoubleDown } from '@iconscout/react-unicons';
 
 const Scroller = () => {
   return (
-    <div className='absolute top-[90%] 2xl:top-[85%] xl:top-[85%] left-[50%] translate-x-[-50%] xl:flex 2xl:flex flex-col hover:cursor-pointer animate-scroll'>
-      <UilMouseAlt
-        className='xs:w-[36px] xs:h-[36px] xl:w-[42px] xl:h-[42px]'
-        color={'#FAFAFA'}
-      />
-      <UilAngleDoubleDown
-        className='xs:w-[36px] xs:h-[36px] xl:w-[42px] xl:h-[42px]'
-        color={'#FAFAFA'}
-      />
-    </div>
+    <a href='#about'>
+      <div className='hidden absolute top-[90%] 2xl:top-[85%] xl:top-[85%] left-[50%] translate-x-[-50%] lg:flex 2xl:flex flex-col hover:cursor-pointer animate-scroll'>
+        <UilMouseAlt className='xs:w-[36px] xs:h-[36px] xl:w-[42px] xl:h-[42px] text-dark dark:text-light' />
+        <UilAngleDoubleDown className='xs:w-[36px] xs:h-[36px] xl:w-[42px] xl:h-[42px] text-dark dark:text-light' />
+      </div>
+    </a>
   );
 };
 
