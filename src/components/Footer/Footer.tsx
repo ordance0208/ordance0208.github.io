e25a9241b2cbe6007183882aecae8b3fc746f69c
Ordan Gramatov <ordance01@yahoo.com>
2023-11-28 13:33:05 +0100

    Refactored footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,35 @@
 import { UilLinkedin, UilGithub, UilFacebook } from '@iconscout/react-unicons';
+import Typography from '../Typography';
 
 const Footer = () => {
   return (
-    <div className='bg-slate-900 p-3 flex flex-col gap-2 items-center justify-center'>
+    <div className='border-t-[1px] border-[#eeeeee] dark:border-t-0 dark:bg-slate-900 p-3 flex flex-col gap-3 items-center justify-center'>
       <div className='flex gap-3'>
-        <UilLinkedin className='text-accent' size={42} />
-        <UilGithub className='text-accent' size={42} />
-        <UilFacebook className='text-accent' size={42} />
+        <a
+          href='https://www.linkedin.com/in/ordan-gramatov/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <UilLinkedin className='text-accent' size={42} />
+        </a>
+        <a
+          href='https://github.com/ordance0208/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <UilGithub className='text-accent' size={42} />
+        </a>
+        <a
+          href='https://www.facebook.com/ordancheg'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <UilFacebook className='text-accent' size={42} />
+        </a>
       </div>
-      <p className='text-[#FAFAFA] '>
+      <Typography className='font-semibold'>
         Copyright &copy; 2023, All rights reserved
-      </p>
+      </Typography>
     </div>
   );
 };
