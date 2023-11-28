10b8241253731f57b60ba835babf821e6d2bbd65
Ordan Gramatov <ordance01@yahoo.com>
2023-11-28 13:32:48 +0100

    Changed button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,22 +5,25 @@ interface Props {
   children?: string | React.ReactNode;
   variant?: 'primary' | 'secondary';
   className?: string;
+  disabled?: boolean;
 }
 
 const Button = ({
   children,
   variant = 'primary',
   className,
+  disabled,
   onClick,
 }: Props) => {
   return (
     <button
-      className={
-        `px-5 py-2 rounded-[11px] font-semibold text-[17px] text-dark hover:opacity-90 transition-colors ${
-          variant === 'primary' ? 'bg-accent text-light' : 'border-2 border-accent dark:text-light'
-        } ` + className
-      }
+      className={`px-5 py-2 rounded-[11px] font-semibold text-dark hover:opacity-90 transition-colors ${
+        variant === 'primary'
+          ? 'bg-accent text-light'
+          : 'border-2 border-accent dark:text-light'
+      } ${className}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
