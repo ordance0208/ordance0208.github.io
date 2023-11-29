a9b16d26fc3e057f5e923fe6ba7faef46b3ba930
Ordan Gramatov <ordance01@yahoo.com>
2023-11-29 17:51:39 +0100

    Render sections in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import Navbar from './components/Navbar';
+import Hero from './sections/Hero';
+import About from './sections/About';
+import Portfolio from './sections/Portfolio';
+import Contact from './sections/Contact/';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div>
-
+    <div className='max-w-[100vw] overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
