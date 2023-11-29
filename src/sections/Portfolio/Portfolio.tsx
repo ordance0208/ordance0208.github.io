c0c755b1340ba307f048c24daaea37d0e233f713
Ordan Gramatov <ordance01@yahoo.com>
2023-11-29 16:11:44 +0100

    Created portfolio section, implemented sub-comps

---
+++ src/sections/Portfolio/Portfolio.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import IProject from '../../models/Project';
+import Background from './components/Background';
+import Container from '../../components/Container';
+import Projects from './components/Projects';
+import ProjectDialog from './components/ProjectDialog';
+
+const Portfolio = () => {
+  const [selectedProject, setSelectedProject] = useState<IProject | null>(null);
+
+  const toggleScroll = () => {
+    if (selectedProject) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  };
+
+  useEffect(() => {
+    toggleScroll();
+    // eslint-disable-next-line
+  }, [selectedProject]);
+
+  return (
+    <>
+      <section
+        id='portfolio'
+        className='bg-light dark:bg-bg-dark relative py-[64px]'
+      >
+        <Background />
+        <Container title='Portfolio' className='flex flex-col h-full pt-5'>
+          <p className='text-accent font-semibold text-[28px] mb-3'>
+            Some of my projects
+          </p>
+          <Projects setSelectedProject={setSelectedProject} />
+        </Container>
+      </section>
+      <ProjectDialog
+        selectedProject={selectedProject}
+        setSelectedProject={setSelectedProject}
+      />
+    </>
+  );
+};
+
+export default Portfolio;
