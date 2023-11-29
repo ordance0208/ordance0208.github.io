255021876622b0263045c95deb921e7d5044503c
Ordan Gramatov <ordance01@yahoo.com>
2023-11-29 17:23:53 +0100

    Modified typography component

--- src/components/Typography/Typograpghy.tsx
+++ src/components/Typography/Typograpghy.tsx
@@ -2,27 +2,28 @@ interface Props {
   children: any;
   variant?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   className?: string;
+  textColor?: string;
 }
 
-const Typograpghy = ({ children, variant = 'p', className }: Props) => {
-  const themeModeClasses = 'text-dark dark:text-light';
+const Typograpghy = ({ children, variant = 'p', className, textColor }: Props) => {
+  const themeModeClasses = `${textColor ? textColor : 'text-dark dark:text-light'} ${className}`;
 
   const renderElement = () => {
     switch (variant) {
       case 'p':
-        return <p className={`${themeModeClasses} ${className}`}>{children}</p>;
+        return <p className={themeModeClasses}>{children}</p>;
       case 'h1':
-        return <h1 className={`${themeModeClasses} ${className}`}>{children}</h1>;
+        return <h1 className={themeModeClasses}>{children}</h1>;
       case 'h2':
-        return <h2 className={`${themeModeClasses} ${className}`}>{children}</h2>;
+        return <h2 className={themeModeClasses}>{children}</h2>;
       case 'h3':
-        return <h3 className={`${themeModeClasses} ${className}`}>{children}</h3>;
+        return <h3 className={themeModeClasses}>{children}</h3>;
       case 'h4':
-        return <h4 className={`${themeModeClasses} ${className}`}>{children}</h4>;
+        return <h4 className={themeModeClasses}>{children}</h4>;
       case 'h5':
-        return <h5 className={`${themeModeClasses} ${className}`}>{children}</h5>;
+        return <h5 className={themeModeClasses}>{children}</h5>;
       case 'h6':
-        return <h6 className={`${themeModeClasses} ${className}`}>{children}</h6>;
+        return <h6 className={themeModeClasses}>{children}</h6>;
     }
   };
 
