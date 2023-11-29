b0220416facb38cd94ed59c5fec3c5de966f71f8
Ordan Gramatov <ordance01@yahoo.com>
2023-11-29 16:09:36 +0100

    Created bg component for about section

--- src/sections/About/components/Background/Background.tsx
+++ src/sections/About/components/Background/Background.tsx
@@ -1,8 +1,8 @@
 const Background = () => {
   return (
     <>
-      <div className='w-[400px] h-[400px] bg-[#eeeeee] absolute top-[50%] left-[70%] blur-[40px] z-0 opacity-5 rounded-full'></div>
-      <div className='w-[400px] h-[400px] bg-[#eeeeee] absolute top-[20%] left-[20%] blur-[40px] z-0 opacity-5 rounded-full'></div>
+      <div className='w-[400px] h-[400px] bg-dark dark:bg-light absolute top-[50%] left-[70%] blur-[40px] z-0 opacity-[0.15] rounded-full'></div>
+      <div className='w-[400px] h-[400px] bg-dark dark:bg-light absolute top-[20%] left-[20%] blur-[40px] z-0 opacity-[0.15] rounded-full'></div>
     </>
   );
 };
