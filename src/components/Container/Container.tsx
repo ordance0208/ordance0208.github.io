033bf1f3faba3b720e17873a48c5600fefab008b
Ordan Gramatov <ordance01@yahoo.com>
2023-11-29 16:10:38 +0100

    Created container utility component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,27 @@
+import Typograpghy from '../Typography/Typograpghy';
+
+interface Props {
+  children: React.ReactNode;
+  title?: string;
+  className?: string;
+}
+
+const Container = ({ title, children, className }: Props) => {
+  return (
+    <div
+      className={`max-w-[90%] m-auto xl:max-w-[1144px] 2xl:max-w-[1440px] ${className}`}
+    >
+      {title && (
+        <Typograpghy
+          variant='h2'
+          className='text-[28px] lg:text-[42px] font-bold mb-2 lg:mb-5'
+        >
+          {title}
+        </Typograpghy>
+      )}
+      {children}
+    </div>
+  );
+};
+
+export default Container;
