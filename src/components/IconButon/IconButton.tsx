29137f11a22e425f78eb0594d70ae891637abaf9
Ordan Gramatov <ordance01@yahoo.com>
2023-11-29 17:24:33 +0100

    Modified icon button styling

--- src/components/IconButon/IconButton.tsx
+++ src/components/IconButon/IconButton.tsx
@@ -1,7 +1,7 @@
 const IconButton = ({ children, onClick, className }: any) => {
   return (
     <div
-      className='p-2 hover:bg-[#3333334d] hover:cursor-pointer rounded-full inline-block'
+      className='p-2 hover:bg-[#3333331A] dark:hover:bg-[#3333334d] hover:cursor-pointer rounded-full inline-block'
       onClick={onClick}
     >
       {children}
