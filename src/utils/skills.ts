9045d9961d46049d856bbb113251f51d9982f805
Ordan Gramatov <ordance01@yahoo.com>
2025-09-25 00:43:14 +0200

    bug fix

--- src/utils/skills.ts
+++ src/utils/skills.ts
@@ -1,7 +1,7 @@
 import Html from '../assets/svg/Html';
 import Css from '../assets/svg/Css';
 import Tailwind from '../assets/svg/Tailwind';
-// import Mui from '../assets/svg/Mui';
+import Mui from '../assets/svg/Mui';
 import Javascript from '../assets/svg/JavaScript';
 import Typescript from '../assets/svg/Typescript';
 import React from '../assets/svg/React';
@@ -27,10 +27,10 @@ const skills = [
     name: 'Tailwind',
     icon: Tailwind,
   },
-  // {
-  //   name: 'Mui',
-  //   icon: Mui,
-  // },
+  {
+    name: 'Mui',
+    icon: Mui,
+  },
   {
     name: 'JavaScript',
     icon: Javascript,
